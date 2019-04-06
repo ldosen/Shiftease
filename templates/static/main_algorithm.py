cb7741a887ba1c2599e8f45dc5f11111180a64c6
Jonathan <warkentin.jon@gmail.com>
2019-04-05 21:09:39 -0500

    implement primary employee sorting

--- templates/static/main_algorithm.py
+++ templates/static/main_algorithm.py
@@ -28,20 +28,40 @@ employees = ["Luke", "George", "Zac"]
 # Also, realistically, there would be conflicts in target shift # per week for each employee, ie not everyone's
 # ideal number can be met. We ain't there yet, so for beginning dev purposes this one happens to fit perfectly.
 
-target_shifts = [1, 1, 2]
+target_shifts = [7, 1, 2]
 
 
-# Step 1.2: Sort the employees into a priority queue based on target # of shifts, and after that based on
-# # of available slots.
+# Step 1.2: Sort the employees into a priority queue based on target # of shifts.
+# TO-DO: if two employees have same number of target shifts, sort those by number of available slots per person.
 
 employees_dict = {}
-
 avbl_index = 0
+emp_index = 0
 
 for employee in employees:
-    employees_dict[employee] = raw_availabilities[avbl_index: avbl_index + total_shifts]
+    employees_dict[employee] = [target_shifts[emp_index],
+                                raw_availabilities[avbl_index: avbl_index + total_shifts]]
     avbl_index += total_shifts
+    emp_index += 1
 
+# Sort employees list by selection sort (almost directly copied from GeeksForGeeks implementation)
+for i in range(len(employees)):
+    # Find the minimum element in remaining
+    # unsorted array
+    min_idx = i
+    for j in range(i+1, len(employees)):
+        if employees_dict[employees[min_idx]][0] > employees_dict[employees[j]][0]:
+            min_idx = j
+
+    # Swap the found minimum element with
+    # the first element
+    employees[i], employees[min_idx] = employees[min_idx], employees[i]
+
+ordered_employees = employees
+employees_queue = queue.Queue(maxsize=len(employees))
+
+for emp in ordered_employees:
+    employees_queue.put(emp)
 
 # Step 2: Step through the schedule and try to schedule someone, checking the constraints to see if its possible.
 # Step 3: Remove the guide from the queue if they can be scheduled
@@ -49,8 +69,11 @@ for employee in employees:
 # Step 5: Update the database with the new schedule information
 # Step 6: ML to make it good
 
+
 def main():
-    print(employees_dict)
+    print("employees_queue values:")
+    while not employees_queue.empty():
+        print(employees_queue.get())
 
 
 if __name__ == '__main__':
