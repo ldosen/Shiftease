c610b216923b03f4e528459e3136948576604093
eamobi <amobisomto@gmail.com>
2019-04-12 15:48:31 -0500

    created simple component for handling 404 not found

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,7 +13,7 @@ render() {
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">Scratch</Link>
+            <Link to="/">Shiftease</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
