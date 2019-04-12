97b638b22faeef0395a045b2aefb456757f34ae8
eamobi <amobisomto@gmail.com>
2019-04-12 15:51:49 -0500

    add notFound error to Routes

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    {/* Catch all unmatched routes if all routes have failed  */}
+    <Route component={NotFound} />
   </Switch>;
