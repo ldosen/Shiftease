1d579f5d4dab1e59f22dc72cf9f86980199ba2b1
eamobi <amobisomto@gmail.com>
2019-04-12 16:01:51 -0500

    created login page

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -6,6 +6,6 @@ import NotFound from "./containers/NotFound";
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
-    {/* Catch all unmatched routes if all routes have failed  */}
+    {/* Catch all unmatched routes if all routes have failed or can't find the routes */}
     <Route component={NotFound} />
   </Switch>;
