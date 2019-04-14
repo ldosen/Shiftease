e63fd1edc02766111acf68d62c07fdac3edf7d48
eamobi <amobisomto@gmail.com>
2019-04-14 15:19:55 -0500

    created Sign up page

---
+++ front-end/src/containers/Signup.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
+import Routes from "../Routes"
+import { LinkContainer } from "react-router-bootstrap";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        username: "",
+        email: "",
+        password: "",
+        first_name: "",
+        last_name: ""
+
+      
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0 && this.state.username.length > 0 && this.state.first_name > 0 && this.state.last_name > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="username" bsSize="large">
+            <ControlLabel>Username</ControlLabel>
+            <FormControl
+              autoFocus
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <FormGroup controlId="first_name" bsSize="large">
+            <ControlLabel>First Name</ControlLabel>
+            <FormControl
+              autoFocus
+              type="text"
+              value={this.state.first_name}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="last_name" bsSize="large">
+            <ControlLabel>Last Name</ControlLabel>
+            <FormControl
+              autoFocus
+              type="text"
+              value={this.state.last_name}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <LinkContainer to="/calendar">
+            <Button
+              block
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+            >
+              Sign up
+          </Button>
+          </LinkContainer>
+        </form>
+      </div>
+    );
+  }
+}
