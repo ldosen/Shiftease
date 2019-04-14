adfcd0c6759b242972d2aebb488376b2c7d9953d
eamobi <amobisomto@gmail.com>
2019-04-14 15:22:10 -0500

    added sign up to routes

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import Calendar from "./containers/Calendar";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
+    <Route path="/sign-up" exact component={Signup} />
     <Route path="/calendar" exact component={Calendar} />
     {/* Catch all unmatched routes if all routes have failed or can't find the routes */}
     <Route component={NotFound} />
