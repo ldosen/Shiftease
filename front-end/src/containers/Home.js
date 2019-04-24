08a37f16d2a84fedca3d0ed6f126328fcb164cc3
MahaBKT <malzanbaqi@luc.edu>
2019-04-24 11:05:35 -0500

    img added

--- front-end/src/containers/Home.js
+++ front-end/src/containers/Home.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+import {img} from "react-bootstrap"
 import "./Home.css";
+import Campus from "../Campus.png";
 
 export default class Home extends Component {
   render() {
     return (
+      
+    
       <div className="Home">
         <div className="lander">
           <h1>Shift Ease</h1>
           <p>A simple employee scheduling app</p>
-        </div>
+          </div>
+        <img src={Campus} width="800" height="400" className="img-responsive"/>
       </div>
     );
   }
